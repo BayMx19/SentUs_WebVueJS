d768397793270a381ff7035d8c2b3b30cd72fc05
BayMx19 <isa.20019@mhs.unesa.ac.id>
2023-05-20 11:50:59 +0700

    Config Netlify

--- src/views/LandingPageView.vue
+++ src/views/LandingPageView.vue
@@ -179,10 +179,9 @@
                     </div>
                 </div>
                 <div class="col-sm-6 ">
-                    <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3957.3489569319318!2d112.72481921477528!3d-7.314639444720972!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7fb7081be4f83%3A0xa9c00ebea3e64b3d!2sSurabaya%20State%20University%20-%20Ketintang%20Campus!5e0!3m2!1sen!2sid!4v1684298083411!5m2!1sen!2sid"
-                        width="400" height="300" style="border:0;" loading="lazy"
-                        referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3957.3489569319318!2d112.72481921477528!3d-7.314639444720972!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7fb7081be4f83%3A0xa9c00ebea3e64b3d!2sSurabaya%20State%20University%20-%20Ketintang%20Campus!5e0!3m2!1sen!2sid!4v1684558216542!5m2!1sen!2sid" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
 
             </div>
