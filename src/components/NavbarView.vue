1ac8b6fc7d86167a8e0b5fabb86b05a8f70f3b38
BayMx19 <isa.20019@mhs.unesa.ac.id>
2023-05-22 11:04:22 +0700

    Update Dashboard

--- src/components/NavbarView.vue
+++ src/components/NavbarView.vue
@@ -1,5 +1,5 @@
 <template>
- <div class="header-area">
+    <div class="header-area">
         <div class="row align-items-center">
 
             <div class="col-md-6 col-sm-4 ">
@@ -8,15 +8,17 @@
                 </div>
 
             </div>
-    
+
             <div class="col-md-6 col-sm-4 clearfix">
                 <ul class="notification-area pull-right">
                     <a class="nav-link nav-profile d-flex align-items-center pe-0" href="/administrator">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-person-circle" viewBox="0 0 16 16">
-  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
-  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
-</svg>
-                        <span class="d-none d-md-block ps-2" style="color:white;">Halo,  !</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white"
+                            class="bi bi-person-circle" viewBox="0 0 16 16">
+                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                            <path fill-rule="evenodd"
+                                d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                        </svg>
+                        <span class="d-none d-md-block ps-2" style="color:white;">Halo, !</span>
                     </a>
                 </ul>
             </div>
